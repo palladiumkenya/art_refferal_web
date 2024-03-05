3e81ed6abdf4c59135882cacb10a3038c9d392de
Ronald-pro <36841157+Ronald-pro@users.noreply.github.com>
2024-03-05 13:12:54 +0300

    fixed error caused by missing protected location_details_id

--- app/Models/FacilityLocationDetail.php
+++ app/Models/FacilityLocationDetail.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class FacilityLocationDetail extends Model
 {
     use HasFactory;
+
     public $table = 'tbl_location_details';
     public $timestamps = false;
+    protected $primaryKey = 'location_details_id';
     protected $fillable = [
         'location_id',
         'location_type',
