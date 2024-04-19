7610e79db69f7e50520190a4040b478bd2a92fef
Kevin Lanyo <kevlanyo@gmail.com>
2024-04-19 14:13:08 +0300

    Track patient VL history

--- app/Models/PatientViralLoad.php
+++ app/Models/PatientViralLoad.php
@@ -5,18 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PatientObservation extends Model
+class PatientViralLoad extends Model
 {
     use HasFactory;
-    public $table = 'tbl_patient_observations';
+    public $table = 'tbl_patient_viral_load';
 
     protected $fillable = [
         'patient_id',
-        'mfl_code',
-        'visit_date',
-        'regimen',
-        'who_stage',
-        'tca',
-        'visit_type',
+        'viral_load',
+        'viral_load_date',
     ];
 }
